df4686965ee5cc006569973d429b1a10c0ee3d95
yuchan <c3686336@gmail.com>
2025-08-12 22:09:45 +0900

    fixup! wip: Use correct scaling value

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@ int main(int argc, char** argv) {
     glfwGetMonitorContentScale(glfwGetPrimaryMonitor(), &xscale, &yscale);
 
     Renderer renderer(
-        static_cast<int>(vidmode->width / 1.7),
+        static_cast<int>(vidmode->width / 1.75),
         static_cast<int>(vidmode->height / 1.75)
     );
 
